0c999592d8e0d59adaaa729a9c5812cec016a524
JorgeBanegas <banegas392@gmail.com>
2021-11-30 13:31:52 -0400

    Agreado nuevo endpoint

--- src/routes/encuestas_firebase.js
+++ src/routes/encuestas_firebase.js
@@ -57,6 +57,38 @@ router.get("/:id", async (req, res)=>{
 });
 
 
+router.get("/list/header", async (req, res)=>{
+    dbRef.child("ModeloEncuesta").get().then((snapshot) => {
+        if (snapshot.exists()) {
+            var json = {};
+            var data = []
+            snapshot.forEach(DataSnapshot => {
+                json = {
+                    id: DataSnapshot.key,
+                    titulo: DataSnapshot.child("Titulo").val(),
+                    descripcion: DataSnapshot.child("Descripcion").val(),
+                    estado: DataSnapshot.child("Estado").val(),
+                }
+                data.push(json);
+            });
+            res.status(200).json(data);
+            
+        } else {
+            res.status(404).json({
+                error: 404,
+                descripcion: "No hay datos disponibles"
+            });
+            console.log("No hay datos disponibles");
+        }
+    }).catch((error) => {
+        res.status(500).json({
+            error: 500,
+            descripcion: "Error en el servidor"
+        });
+        console.error(error);
+    });
+});
+
 
 module.exports = router;
  
